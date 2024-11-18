97104e9afc8f2317986f1ef0219318941098198a
Saymon Shoab <76821250+saymon-shoab@users.noreply.github.com>
2024-11-18 20:39:28 +0600

    spacex api integation

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+
+<svelte:head>
+<link rel="stylesheet" href="https://unpkg.com/flowbite@1.3.2/dist/flowbite.min.css" /></svelte:head> 
+<slot />
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
