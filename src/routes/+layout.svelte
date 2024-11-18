9d3bd5613a32baffe4e7e26997e2a470569f33f1
Saymon Shoab <76821250+saymon-shoab@users.noreply.github.com>
2024-11-19 03:05:53 +0600

    code refactoring

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,24 +6,21 @@
     NavBrand,
     NavLi,
     NavUl,
-    NavHamburger,
-    Button,
   } from "flowbite-svelte";
 </script>
 
-<div class="container mx-auto p-4">
+<div class="container p-4">
   <Navbar>
     <NavBrand href="/">
       <h1 class="text-2xl font-bold my-4">SpaceX Launches</h1>
     </NavBrand>
-    <div class="flex md:order-2">
-      <Button size="sm">Get started</Button>
-      <NavHamburger />
+    <div class=" md:order-2">
+      <NavUl class="order-1">
+        <NavLi href="/" active={true}>Home</NavLi>
+        <NavLi href="/map">Map</NavLi>
+      </NavUl>
     </div>
-    <NavUl class="order-1">
-      <NavLi href="/" active={true}>Home</NavLi>
-      <NavLi href="/map">Open Layer</NavLi>
-    </NavUl>
+
   </Navbar>
   <slot />
 </div>
